b19503b6b18ca4c7231509750a1cd7f9964e9a3e
user <user@gmail.com>
2016-09-01 11:33:17 +0800

    Beta Version

--- app/src/main/java/com/example/yiyang/bluetoothtest/MainActivity.java
+++ app/src/main/java/com/example/yiyang/bluetoothtest/MainActivity.java
@@ -2,28 +2,40 @@ package com.example.yiyang.bluetoothtest;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private static final int REQUEST_ENABLE_BT = 1234;
     private static final int REQUEST_ENABLE_BT_DISCOVERABLE = 1235;
+    private static final int BT_MESSAGE_READ = 1236;
+    private static final String BT_SDP_SERVICE_NAME = "BluetoothTest";
+    private static final String BT_SDP_UUID = "5cdbe98a-6fdd-11e6-8b77-86f30ca893d3";
     private BluetoothAdapter btAdapter;
     private Button btnGetBTA;
     private Button btnEnableBT;
@@ -37,6 +49,19 @@ public class MainActivity extends AppCompatActivity {
     private Button btnDiscoverBT;
     private Button btnDiscoverable;
     private TextView tvDiscoverable;
+    private ConnectedThread connectedThread;
+    private AcceptThread acceptThread;
+    private Button btnListen;
+    private TextView tvListen;
+    private Set<BluetoothDevice> pairedDevices;
+    private Button btnSayHello;
+    private EditText etSayHello;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        connectedThread.cancel();
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -53,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
                 btnEnableBT.setText("Enable BT");
 
             }
-        } else if (requestCode == REQUEST_ENABLE_BT_DISCOVERABLE){
+        } else if (requestCode == REQUEST_ENABLE_BT_DISCOVERABLE) {
 
             if (resultCode == RESULT_OK) {
                 tvDiscoverable.setText("BT DISCOVERABLE!");
@@ -90,6 +115,10 @@ public class MainActivity extends AppCompatActivity {
         registerReceiver(mReceiver, filter);
 
 
+        acceptThread = new AcceptThread();
+//        acceptThread.start();
+
+
     }
 
     private void setListeners() {
@@ -104,7 +133,24 @@ public class MainActivity extends AppCompatActivity {
         lvPairedDevices.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this, ((TextView) view).getText(), Toast.LENGTH_LONG).show();
+
+                String data = ((TextView) view).getText().toString();
+                String[] items = data.split("\n");
+
+                BluetoothDevice btDevice = null;
+                for (BluetoothDevice btd :
+                        pairedDevices) {
+                    if (btd.getAddress().equals(items[1])) {
+                        btDevice = btd;
+                        break;
+                    }
+                }
+
+                if (btDevice != null) {
+                    ConnectThread connectThread = new ConnectThread(btDevice);
+                    connectThread.start();
+                }
+
 
                 return true;
             }
@@ -113,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
         btnGetPairedDevices.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+                pairedDevices = btAdapter.getBondedDevices();
                 // If there are paired devices
                 if (pairedDevices.size() > 0) {
                     arrPairedDeviceList.clear();
@@ -172,6 +218,30 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        btnListen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (btnListen.getText().equals("Listen")) {
+                    acceptThread.start();
+                    btnListen.setText("Stop Listen");
+                    tvListen.setText("Listening");
+                } else {
+                    acceptThread.cancel();
+                    btnListen.setText("Listen");
+                    tvListen.setText("");
+                }
+            }
+        });
+
+
+        btnSayHello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                byte[] data = etSayHello.getText().toString().getBytes();
+                connectedThread.write(data);
+            }
+        });
     }
 
     private void fineViews() {
@@ -186,6 +256,10 @@ public class MainActivity extends AppCompatActivity {
         btnDiscoverBT = (Button) findViewById(R.id.btnDiscoverBT);
         btnDiscoverable = (Button) findViewById(R.id.btnDiscoverable);
         tvDiscoverable = (TextView) findViewById(R.id.tvDiscoverable);
+        btnListen = (Button) findViewById(R.id.btnListen);
+        tvListen = (TextView) findViewById(R.id.tvListen);
+        btnSayHello = (Button) findViewById(R.id.btnSayHello);
+        etSayHello = (EditText) findViewById(R.id.etSayHello);
     }
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
@@ -221,6 +295,188 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+
+    private class AcceptThread extends Thread {
+        private final BluetoothServerSocket mmServerSocket;
+
+        public AcceptThread() {
+            // Use a temporary object that is later assigned to mmServerSocket,
+            // because mmServerSocket is final
+            BluetoothServerSocket tmp = null;
+            try {
+                // BT_SDP_UUID is the app's UUID string, also used by the client code
+                tmp = btAdapter.listenUsingRfcommWithServiceRecord(BT_SDP_SERVICE_NAME
+                        , UUID.fromString(BT_SDP_UUID));
+            } catch (IOException e) {
+            }
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            BluetoothSocket socket = null;
+            // Keep listening until exception occurs or a socket is returned
+            while (true) {
+                try {
+                    socket = mmServerSocket.accept();
+                } catch (IOException e) {
+                    break;
+                }
+                // If a connection was accepted
+                if (socket != null) {
+                    // Do work to manage the connection (in a separate thread)
+                    manageConnectedSocket(socket);
+                    try {
+                        mmServerSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
+        }
+
+        /**
+         * Will cancel the listening socket, and cause the thread to finish
+         */
+        public void cancel() {
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket,
+            // because mmSocket is final
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
+            try {
+                // BT_SDP_UUID is the app's UUID string, also used by the server code
+                tmp = device.createRfcommSocketToServiceRecord(UUID.fromString(BT_SDP_UUID));
+            } catch (IOException e) {
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it will slow down the connection
+            btAdapter.cancelDiscovery();
+
+            try {
+                // Connect the device through the socket. This will block
+                // until it succeeds or throws an exception
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and get out
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                }
+                return;
+            }
+
+            // Do work to manage the connection (in a separate thread)
+            manageConnectedSocket(mmSocket);
+        }
+
+        /**
+         * Will cancel an in-progress connection, and close the socket
+         */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case BT_MESSAGE_READ:
+                    String data = new String((byte[]) msg.obj).trim();
+                    Toast.makeText(MainActivity.this, data, Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+    };
+
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+                    // Send the obtained bytes to the UI activity
+                    mHandler.obtainMessage(BT_MESSAGE_READ, bytes, -1, buffer)
+                            .sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+            }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+
+    private void manageConnectedSocket(BluetoothSocket socket) {
+        connectedThread = new ConnectedThread(socket);
+        connectedThread.start();
+//        tvListen.setText("Connected");
+//        Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_LONG).show();
+    }
 
 
 }
